bfb67d0ba92dde5bf916ef55f2ce6c4b33a9f856
joelamoakoh <20096482@mail.wit.ie>
2023-03-22 08:42:52 +0000

    Using lambdas counting

--- src/main/kotlin/controllers/NoteApi.kt
+++ src/main/kotlin/controllers/NoteApi.kt
@@ -11,17 +11,12 @@ class NoteAPI (serializerType: Serializer) {
     fun add(note: Note): Boolean {
         return notes.add(note)
     }
-    fun listAllNotes(): String {
-        return if (notes.isEmpty()) {
-            "NO NOTES STORED"
-        } else {
-            var listOfNotes = ""
-            for (i in notes.indices) {
-                listOfNotes += "${i}: ${notes[i]} \n"
-            }
-            listOfNotes
-        }
-    }
+    fun listAllNotes(): String =
+        if  (notes.isEmpty()) "No notes stored"
+        else notes.joinToString (separator = "\n") { note ->
+            notes.indexOf(note).toString() + ": " + note.toString() }
+
+
     fun numberOfNotes(): Int {
         return notes.size
     }
